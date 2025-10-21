ad8855aa7e236b30629c54539e60b0cbadbdcef1
FIDHA <fidhasherin.eranhikkal@gmail.com>
2025-10-21 23:38:41 +0530

    initial commit

---
+++ day 1/var.js
@@ -0,0 +1,132 @@
+// let name ="fidah"
+// console.log(name);
+// document.getElementById("first").innerHTML =name;
+
+// 2. Create two variables, a = 5 and b = 10. Print their sum, difference, and product
+//  let a = 5;
+//  let b =10;
+//  console.log(a+b);
+//  console.log(a-b);
+//  console.log(a*b);
+//  console.log(b/a)
+
+// 5. Declare three variables in one line and assign them values: firstName, lastName, and age.
+//  let name =["FirstName:fidha", "LastName:sherin ek", "age:21"]
+
+//  for( let i=0; i<name.length; i++){
+//  console.log(name[i]);
+//  document.getElementById("first").innerHTML +=name[i] + "<br>";
+//  }
+ 
+//  console.log(name);
+//  document.getElementById("first").innerHTML = name;
+
+
+// 7. Predict the output:
+// var color = 'red';
+// var color = 'blue';
+// console.log(color);
+ 
+// 8. Predict the output:
+// let fruit = 'apple';
+// let fruit = 'banana';
+// console.log(fruit);
+
+// 9. Predict:
+// var x = 1;
+// {
+// var x = 2;
+// }
+// console.log(x)
+
+
+// 10. Predict:
+// let y = 1;
+// {
+// let y = 2;
+// }
+// console.log(y);  
+// 11. What will be the output?
+// console.log(a);
+// var a = 10;
+
+// 2. What about this?
+// console.log(b);
+// let b = 10;
+
+// var a = 100;
+// function test() {
+// var a = 200;
+// console.log(a);
+// }
+// test();
+// console.log(a)
+// 5. Predict:
+// let message = 'Hello';
+// function say() {
+// console.log(message);
+// let message = 'Hi';
+// }
+// say();
+// let name = prompt("what is your name");
+// console.log("my name is" + name + "!welcome to java script")
+// alert("welcome" + name + "to javascript")
+// const person ={name:"fidha", age:20};
+// person.age =21;
+// console.log(person);
+// document.getElementById("first").innerHTML = "name "+person.name+ "<br>"+ "age" +  person.age;
+// 8. Why does this cause an error?
+// const person = { name: 'John', age: 30 };
+// person = { name: 'Jane', age: 25 };
+
+// // 9. Predict:
+// let x = 5;
+// let y = x;
+// x = 10;
+// console.log(y);
+
+// 0. Predict:
+// const arr = [1, 2, 3];
+// arr.push(4);
+// console.log(arr);
+// let x ="heelo world";
+// console.log(typeof x)
+// let  a =2025;
+// console.log(typeof a);
+// let b = true;
+//  console.log(typeof b);
+//  let c;
+//  console.log(typeof c);
+//  let e = null;
+//  console.log(typeof e);
+//  let f = { name: 'Fidha', age: 20 };
+//  console.log(typeof f);
+//  let g =  [1, 2, 3, 4];
+//  console.log(typeof g);
+//  let h = 9007199254740991n;
+//  console.log(typeof h);
+//  function greet() {
+//   console.log("Hello, world!");
+// }
+// console.log(typeof greet); 
+
+// let x = 0;
+// x =Boolean(0);
+// let person = {name: 'Sara', age: 21}; 
+// console.log(person.name);
+// let arr =[10, 20, 20];
+// console.log(arr[1]);
+// let obj ={a:1, b:2};
+// delete obj.a;
+// console.log(obj);
+// console.log(typeof[])
+// console.log(Array.isArray({}));
+// console.log(Array.isArray([]));
+
+ 
+// console.log(typeof typeof 1);
+// console.log(null+1);
+// console.log([]+[]);
+// console.log([]==![]);
+// console.log('2'+2-2);
+// console.log("10"-"4"-"3"-2 +"5");
